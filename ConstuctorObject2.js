e8bae0a1ab24774ad30ea88315ab0b811459fe57
Ipung Purwono <ipungofficial@gmail.com>
2020-05-04 14:07:45 +0700

    Contructor Object2

---
+++ ConstuctorObject2.js
@@ -0,0 +1,37 @@
+function Mahasiswa(nim, nama, kelas, umur, semester, matakuliah) {
+    //property
+    this.profil = {
+        nim: nim,
+        nama: nama
+    };
+    this.kelas = kelas
+    this.umur = umur
+    this.semester = semester
+    this.matakuliah = matakuliah
+
+    //method
+    this.profilmahasiswa = function () {
+        console.log(
+            'Nim: ' +
+            this.profil.nim + ' ' +
+            'Nama: ' + ' ' +
+            this.profil.nama + ' ' +
+            'Umur: ' + ' ' +
+            this.umur + ' Tahun' + ' ' +
+            'Mata Kuliah Dipilih: ' + ' ' +
+            this.matakuliah[0] + ' dan ' + ' ' +
+            this.matakuliah[1] + '.');
+    };
+
+}
+
+let mahasiswa1 = new Mahasiswa(
+    '201801138',
+    'Eva Ratna Palupi',
+    'Kelas B',
+    20,
+    6,
+    ['Pemrograman Website', 'Manajemen Proyek SI']
+)
+
+mahasiswa1.profilmahasiswa()
